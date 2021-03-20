5a84deca6c20828b1173471423a6470cf0f92a50
gemosant <rasim.mutlu@outlook.com>
2021-03-20 12:54:01 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Componenents/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
